e18361bf77702c63db684f9c6f1d2373c8e93bfd
Florian Bortolotti <flori@MSI>
2025-06-06 20:10:44 +0200

    version finale du bot

--- bot.py
+++ bot.py
@@ -1,25 +1,20 @@
 import discord
-from discord.ext import commands
+import time
 import os
 
-# Pour charger les variables d'environnement depuis un fichier .env (optionnel mais recommandé en local)
+# Pour charger les variables d'environnement depuis un fichier .env
 from dotenv import load_dotenv
 load_dotenv()  # charge les variables depuis un fichier .env s'il existe
 
 bot = discord.Client(intents=discord.Intents.all()) 
 
-# Remplace ceci par l'ID de l'utilisateur qui déclenche le mute
 ID_DU_GOAT = 239746340559650816
+
+ID_DU_DOG = 216143911314391042
 
 @bot.event
 async def on_ready():
     print(f'Bot connecté en tant que {bot.user}')
-
-@bot.event
-async def on_message(message: discord.Message):
-    channel = message.channel
-    if message.content == "i" and channel.id == 1283064453386080402 :
-        await channel.send("ratio loser")
 
 @bot.event
 async def on_voice_state_update(member, before, after):
@@ -28,10 +23,13 @@ async def on_voice_state_update(member, before, after):
         if member.id == ID_DU_GOAT:
             channel = after.channel
             for m in channel.members:
-                if m.id != ID_DU_GOAT and not m.bot:
+                if m.id != ID_DU_GOAT and not m.bot and m.id == ID_DU_DOG:
                     try:
                         await m.edit(mute=True)
-                        print(f'Muted: {m.name}')
+                        print(f'Muselière appliquée: {m.name}')
+                        time.sleep(3)
+                        await m.edit(mute=False)
+                        print(f'Muselière retirée: {m.name}')
                     except Exception as e:
                         print(f"Erreur en mutant {m.name}: {e}")
 
